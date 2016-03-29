8fb15088bc0600ea9306643cbdaa2a45766c366f
Kevin Cantwell <kevin.cantwell@gmail.com>
2016-03-29 16:31:00 -0400

    test with https api

--- README.md
+++ README.md
@@ -28,3 +28,27 @@ $ PORT=5099 go run main.go 100000
 Without drain (x100000): 5.692069876s
    With drain (x100000): 5.648899366s
 ```
+
+# UPDATE
+
+Thanks to [@shurcooL's](https://github.com/shurcooL) [comment](https://github.com/google/go-github/pull/317#issuecomment-203017726) I updated this example to use a TLS connection, which is really where this problem is illuminated. TLS connections are much more expensive to reopen and therefore gain more benefit from reuse.
+
+I've updated my example to just hit Github's api over https:
+
+```
+$ go version
+go version go1.6 darwin/amd64
+
+$ go run main.go https://api.github.com/repos/google/go-github 10
+Without drain (x10): 1.302103099s
+   With drain (x10): 282.559111ms
+
+$ go run main.go https://api.github.com/repos/google/go-github 100
+Without drain (x100): 8.339789738s
+   With drain (x100): 2.694648312s
+
+$ go run main.go https://api.github.com/repos/google/go-github 1000
+Without drain (x1000): 1m16.253147895s
+   With drain (x1000): 32.260955511s
+```
+
--- main.go
+++ main.go
@@ -12,35 +12,25 @@ import (
 )
 
 func main() {
-	n, _ := strconv.Atoi(os.Args[1])
+	url := os.Args[1]
+	n, _ := strconv.Atoi(os.Args[2])
 
-	go func() {
-		time.Sleep(1 * time.Second) // Give the web server plenty of time to begin serving requests
+	client := http.DefaultClient
 
-		client := http.DefaultClient
+	d1 := run(client, url, n, false)
+	d2 := run(client, url, n, true)
 
-		d1 := testWithoutDrain(client, n)
-		d2 := testWithDrain(client, n)
+	fmt.Printf("Without drain (x%d): %v\n", n, d1)
+	fmt.Printf("   With drain (x%d): %v\n", n, d2)
 
-		fmt.Printf("Without drain (x%d): %v\n", n, d1)
-		fmt.Printf("   With drain (x%d): %v\n", n, d2)
-
-		os.Exit(0)
-	}()
-
-	http.HandleFunc("/some.json", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("{}\n\n")) // Adds extra bytes to the json response
-	})
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		panic(err)
-	}
+	os.Exit(0)
 }
 
-func testWithoutDrain(client *http.Client, n int) time.Duration {
+func run(client *http.Client, url string, n int, drain bool) time.Duration {
 	var total time.Duration
 	for i := 0; i < n; i++ {
 		start := time.Now()
-		resp, err := client.Get("http://localhost:" + os.Getenv("PORT") + "/some.json")
+		resp, err := client.Get(url)
 		if err != nil {
 			panic(err)
 		}
@@ -52,30 +42,9 @@ func testWithoutDrain(client *http.Client, n int) time.Duration {
 		timing := time.Since(start)
 		total += timing
 
-		// Don't drain, just close
-		resp.Body.Close()
-	}
-	return total
-}
-
-func testWithDrain(client *http.Client, n int) time.Duration {
-	var total time.Duration
-	for i := 0; i < n; i++ {
-		start := time.Now()
-		resp, err := client.Get("http://localhost:" + os.Getenv("PORT") + "/some.json")
-		if err != nil {
-			panic(err)
+		if drain {
+			io.Copy(ioutil.Discard, resp.Body)
 		}
-		dec := json.NewDecoder(resp.Body)
-		var body map[string]interface{}
-		if err := dec.Decode(&body); err != nil {
-			panic(err)
-		}
-		timing := time.Since(start)
-		total += timing
-
-		// Drain and close
-		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
 	}
 	return total
